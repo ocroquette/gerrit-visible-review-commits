a42cef94057aba4bf9a391e237bf32c376b79753
Olivier Croquette <ocroquette@free.fr>
2024-10-19 09:00:13 +0200

    Fix build issue in PluginCommandModule with newer JDK
    VisibleReviewCommitsCommandModule.java:5: error:
      constructor PluginCommandModule in class PluginCommandModule cannot be applied to given types;
        class VisibleReviewCommitsCommandModule extends PluginCommandModule {

--- src/main/java/com/googlesource/gerrit/plugins/visiblereviewcommits/VisibleReviewCommitsCommandModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/visiblereviewcommits/VisibleReviewCommitsCommandModule.java
@@ -1,11 +1,17 @@
 package com.googlesource.gerrit.plugins.visiblereviewcommits;
-
+ 
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.sshd.PluginCommandModule;
-
+import com.google.inject.Inject;
+ 
 class VisibleReviewCommitsCommandModule extends PluginCommandModule {
+  @Inject
+  VisibleReviewCommitsCommandModule(@PluginName String pluginName) {
+    super(pluginName);
+  }
+ 
   @Override
   protected void configureCommands() {
     command(SshCommandRefresh.class);
   }
 }
-
